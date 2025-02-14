2f75710f2ff65833cb2bc1d3868a3900a729a5c8
Steinar <steinarb20@ru.is>
2025-02-14 23:17:54 +0000

    completed multistep model for option pricing

--- manual.py
+++ manual.py
@@ -1,6 +1,10 @@
 import numpy as np
 from dataclasses import dataclass
 
+# ===========================================
+#                 Functions
+# ===========================================
+
 def terminal_value_call(S_T, K):
     return max(0, S_T - K)
 
@@ -10,25 +14,11 @@ def terminal_value_put(S_T, K):
 @dataclass
 class Node:
     S: float
-    option_value: float
+    option_value: float = None
 
-
-# class Node:
-#     def __init__(self, S, t, terminal_value):
-#         if t == T: # Terminal node
-#             self.up = None
-#             self.down = None
-#             self.option_value = terminal_value(S, K)
-#         else:
-#             self.up = Node(S * u, t + dt, terminal_value)
-#             self.down = Node(S * d, t + dt, terminal_value)
-#             self.option_value = np.exp(-r * dt) * (q_u * self.up.option_value + q_d * self.down.option_value)
-
-#         self.S = S
-#         self.t = t
-
-#     def y(self):
-#         pass
+# ===========================================
+#                 Parameters
+# ===========================================
 
 S_0 = 100
 K = 100
@@ -37,26 +27,51 @@ sigma = 0.2
 T = 1
 steps = 1
 
+# ===========================================
+#                    Main
+# ===========================================
+
 dt = T / steps
 u = np.exp(sigma * np.sqrt(dt))
 d = 1 / u
 q_u = (np.exp(r * dt) - d) / (u - d)
 q_d = 1 - q_u
 
-
+# Forward pass, fill in stock prices
 binary_tree = [
     [
         Node(
-            S = S_0 * (u**t) * (d**(k-t)),
-            option_value = 0,
+            S = S_0 * (u**(t-k)) * (d**(k)),
         )
      for k in range(t+1)] 
     for t in range(steps+1)
 ]
 
-print(u, d)
-print(binary_tree)
+# Backward pass, fill in terminal option values
+binary_tree[steps] = [
+    Node(
+        S = node.S,
+        option_value = terminal_value_call(node.S, K)
+    )
+    for node in binary_tree[steps]
+]
 
+# Backward pass, fill in option values
+for t in range(steps-1, -1, -1):
+    for k in range(t+1):
+        binary_tree[t][k].option_value = np.exp(-r * dt) * (
+            q_u * binary_tree[t+1][k].option_value + 
+            q_d * binary_tree[t+1][k+1].option_value
+        )
+
+
+print(f"u: {u}")
+print(f"d: {d}")
+# Print tree
+for i in range(steps+1):
+    for j in range(i+1):
+        print(f"S: {binary_tree[i][j].S:.2f}, V: {binary_tree[i][j].option_value:.2f}", end=" | ")
+    print()
 
 
 
